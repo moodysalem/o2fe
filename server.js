352246a3d357871abfa0a5926484bc493404ae83
Moody Salem <moody.salem@gmail.com>
2015-12-12 01:28:33 -0600

    the server stuff

--- server.js
+++ server.js
@@ -2,12 +2,39 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+var env = process.env;
+
+/**
+ * Serve all static files on the root
+ */
 app.use('/', express.static('dist'));
 
+/**
+ * Use prerender.io for SEO
+ */
+app.use(require('prerender-node').set('prerenderToken', env.PRERENDER_TOKEN));
+
+/**
+ * Pass the configuration stored in environment variables to the client via a script tag
+ */
+app.get('/config.js', function (req, res) {
+  res.setHeader('Content-Type', 'application/javascript');
+  res.send('var config = ' + JSON.stringify({
+      API_URL: env.API_URL,
+      CLIENT_ID: env.CLIENT_ID
+    }) + ';');
+});
+
+/**
+ * Send all requests for other files to index html
+ */
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
+/**
+ * Serve on 3000
+ */
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
