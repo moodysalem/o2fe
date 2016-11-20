a826cc866ab7b3c041baf9e74e80f0f894b4a767
moodysalem <a454465c8beef70811a01489042c79b7f235df5e>
2016-11-20 10:36:56 -0600

    improve docs with links and add some pricing info

--- src/js/pages/Docs.jsx
+++ src/js/pages/Docs.jsx
@@ -2,6 +2,7 @@ import React, {PropTypes, Component, PureComponent} from "react";
 import {CONFIG_SHAPE} from "../util/shapes";
 import join from "url-join";
 import _ from "underscore";
+import {Link} from "react-router";
 
 export default class Docs extends PureComponent {
   static propTypes = {
@@ -50,20 +51,30 @@ export default class Docs extends PureComponent {
     }
   };
 
+  handleClick(to) {
+    this.scrollToSection(to);
+  }
+
   render() {
     const {config} = this.context;
 
+    const HL = ({to}) => (
+      <Link className="visible-on-parent-hover" to={join('docs', to)} onClick={() => this.handleClick(to)}>
+        <i className="fa fa-link grey-text"/>
+      </Link>
+    );
+
     return (
-      <div className="container">
+      <div className="container" style={{marginBottom: 60}}>
         <h1 ref="top" className="overflow-ellipsis">Docs</h1>
-        <h2 ref="intro" className="overflow-ellipsis">Introduction</h2>
+        <h2 ref="intro" className="overflow-ellipsis">Introduction <HL to="intro"/></h2>
         <p className="flow-text">
           This specification describes the functionality and the expected behavior of this OAuth2 server.
 
           For more general information about OAuth2, check out
           the <a target="_blank" rel="noopener" href="https://tools.ietf.org/html/rfc6749">specification</a>.
         </p>
-        <h2 ref="api" className="overflow-ellipsis">API</h2>
+        <h2 ref="api" className="overflow-ellipsis">API <HL to="api"/></h2>
         <p className="flow-text">
           Live documentation for the API can be found
           at <a target="_blank" rel="noopener" href="https://docs.oauth2cloud.com/#/oauth2">docs.oauth2cloud.com</a>.
@@ -75,39 +86,39 @@ export default class Docs extends PureComponent {
         </p>
         <hr />
 
-        <h2 ref="functionality" className="overflow-ellipsis">Functionality</h2>
+        <h2 ref="functionality" className="overflow-ellipsis">Functionality <HL to="functionality"/></h2>
 
-        <h3 ref="authentication" className="overflow-ellipsis">Authentication</h3>
+        <h3 ref="authentication" className="overflow-ellipsis">Authentication <HL to="authentication"/></h3>
         <p className="flow-text">
           This OAuth2 server does not allow log in via e-mail and password combinations. Rather the only methods
           available for user authentication are via e-mail or third party provider (e.g. Google or Facebook.)
         </p>
-        <h4 ref="email" className="overflow-ellipsis">Via E-mail</h4>
+        <h4 ref="email" className="overflow-ellipsis">Via E-mail <HL to="email"/></h4>
         <p>
           The user enters an e-mail address and receives an e-mail with a link to log in. The duration for which this
           link will work is configurable. Once clicked, the user is directed to a page where they may authorize any
           client requested scopes.
         </p>
 
-        <h3 ref="provider-log-in">Providers</h3>
+        <h3 ref="provider-log-in">Providers <HL to="provider-log-in"/></h3>
         <p className="flow-text">
           In order for users to be able to log in via Facebook and Google, you must create corresponding clients in the
           Facebook and Google developer consoles respectively.
         </p>
-        <h4 ref="google" className="overflow-ellipsis">Via Google</h4>
+        <h4 ref="google" className="overflow-ellipsis">Via Google <HL to="google"/></h4>
         <p>
           The user clicks a link where they are directed to sign in via Google. Once logged in to Google, they will be
           redirected back to OAuth2Cloud to authorize any client-requested scopes.
         </p>
 
-        <h4 ref="facebook" className="overflow-ellipsis">Via Facebook</h4>
+        <h4 ref="facebook" className="overflow-ellipsis">Via Facebook <HL to="facebook"/></h4>
         <p>
           The user clicks a link where they are directed to sign in via Facebook. Once logged in to Facebook, they will
           be redirected back to OAuth2Cloud to authorize any client-requested scopes. The 'email' scope must be used
           in order to log in via OAuth2Cloud.
         </p>
 
-        <h3 ref="authorization" className="overflow-ellipsis">Authorization</h3>
+        <h3 ref="authorization" className="overflow-ellipsis">Authorization <HL to="authorization"/></h3>
         <p>
           This OAuth2 server supports defining scopes and assigning scopes to different clients with three different
           priorities.
@@ -119,7 +130,8 @@ export default class Docs extends PureComponent {
             continue.
           </li>
           <li>
-            <strong>Required &amp; Hidden</strong>: The user will not be prompted to authorize this scope, but any tokens
+            <strong>Required &amp; Hidden</strong>: The user will not be prompted to authorize this scope, but any
+            tokens
             created will have the scope. The user will not be shown details about the scope.
           </li>
           <li>
@@ -127,7 +139,7 @@ export default class Docs extends PureComponent {
           </li>
         </ul>
 
-        <h3 ref="user-linking" className="overflow-ellipsis">Linking Users</h3>
+        <h3 ref="user-linking" className="overflow-ellipsis">Linking Users <HL to="user-linking"/></h3>
         <p>
           Because we cannot merge your user-associated data, we allow you to link users via this administrative website.
         </p>
@@ -138,14 +150,14 @@ export default class Docs extends PureComponent {
         </p>
 
         <hr />
-        <h2 ref="customization" className="overflow-ellipsis">Customizing your Log In Page</h2>
-        <h3 ref="style" className="overflow-ellipsis">Style</h3>
+        <h2 ref="customization" className="overflow-ellipsis">Customizing your Log In Page <HL to="customization"/></h2>
+        <h3 ref="style" className="overflow-ellipsis">Style <HL to="style"/></h3>
         <p className="flow-text">
           OAuth2Cloud supports injecting your own stylesheets, favicons, and logo URLs into your authentication and
           authorization pages
         </p>
 
-        <h3 ref="domain" className="overflow-ellipsis">Using your own domain</h3>
+        <h3 ref="domain" className="overflow-ellipsis">Using your own domain <HL to="domain"/></h3>
         <p className="flow-text">
           You can send users to a log in page on your own domain, e.g. login.my-site.com
         </p>
@@ -154,8 +166,6 @@ export default class Docs extends PureComponent {
           <li>Configure your CDN to serve your own SSL certificates</li>
           <li>Point your DNS at the CDN-provided domain</li>
         </ol>
-
-        <div style={{marginBottom: 60}}/>
       </div>
     );
   }
--- src/js/pages/Pricing.jsx
+++ src/js/pages/Pricing.jsx
@@ -5,9 +5,30 @@ export default class Pricing extends PureComponent {
     return (
       <div className="container">
         <h1>Pricing</h1>
-        <p className="flow-text">
-          OAuth2Cloud is a beta service.
-          For all pricing inquiries, please contact <a href="mailto:moody.salem@gmail.com">moody.salem@gmail.com</a>.
+
+        <table className="repsonsive-table bordered centered">
+          <thead>
+          <tr>
+            <th># Users</th>
+            <th>Pricing ($USD/month)</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td>&lt; 1000</td>
+            <td>Free Forever</td>
+          </tr>
+          <tr>
+            <td>&gt;= 1000</td>
+            <td>
+              <a href="mailto:moody.salem@gmail.com?subject=OAuth2Cloud Pricing Inquiry">Contact Us</a>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+
+        <p className="grey-text">
+          OAuth2Cloud is a beta service and the pricing model is subject to change.
         </p>
       </div>
     );
