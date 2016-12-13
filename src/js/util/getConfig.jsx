4204cd25d5eae58495bf6408f3590d15a001e0b9
Moody Salem <moodysalem@users.noreply.github.com>
2016-12-13 14:05:16 -0600

    Update getConfig.jsx

--- src/js/util/getConfig.jsx
+++ src/js/util/getConfig.jsx
@@ -2,10 +2,10 @@ let _config = null;
 
 export default function getConfig() {
   if (_config != null) {
-    return Promise.resolve(_config);
+    return _config;
   }
 
-  return fetch('config.json')
+  _config = fetch('config.json')
     .then(
       res => {
         if (res.ok) {
@@ -20,8 +20,9 @@ export default function getConfig() {
           return Promise.reject(new Error('Config was missing either API_URL or CLIENT_ID or both'));
         }
 
-        _config = config;
-        return _config;
+        return config;
       }
     );
+  
+  return _config;
 }
